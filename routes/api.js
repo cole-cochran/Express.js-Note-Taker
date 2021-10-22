1eba60d2ce856ae6ddbcaaf0a158f508d957ad25
Cole Cochran <colecochran405@gmail.com>
2021-10-22 15:20:48 -0500

    building the app.post

--- routes/api.js
+++ routes/api.js
@@ -26,3 +26,33 @@ app.get('/notes', (req,res)=>{
     
 }
 )
+
+app.post('/notes',(req,res)=>{
+    console.log('be warned \n your pathetic attempt has been noticed')
+
+    let filePost = path.join(__dirname, "../db/db.json");
+
+    
+    let noteDB = req.body
+
+    noteDB.id = uuidGEN()
+    
+    console.log(`The note now has a new destination:${noteDB.id}`)
+
+  
+     db.push(noteDB)
+    
+    fs.writeFile(filePost, JSON.stringify(db), (err)=> {
+        if(err){
+           console.log(err)
+        }else{
+           console.log('fortune favored you this time boy.');
+        } 
+      });
+    
+      
+     res.json(noteDB);
+}
+)
+
+module.exports = app;
