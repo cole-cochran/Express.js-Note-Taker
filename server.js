1232f28373dcc9f546ee5c1b955e6cbedfd69871
Cole Cochran <colecochran405@gmail.com>
2021-10-22 20:23:58 -0500

    finishing the math for uuidGEN and cleaning up the console.log

--- server.js
+++ server.js
@@ -3,3 +3,17 @@ const fs = ('fs');
 const db = ('.db/db/json');
 const path = require('path');
 
+const apiRoutes = require('./routes/api');
+const htmlRoutes = require('./routes/html');
+
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const PORT= process.env.PORT || 3001;
+
+app.use(express.static('public'));
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
